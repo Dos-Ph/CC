f82427234479d41182f0cb7e745914f37ac05d3a
supicha <supichapd@gmail.com>
2015-10-23 12:58:09 +0700

    start project

---
+++ app/src/main/java/converter/color/dos/com/colorconverter/MainActivity.java
@@ -0,0 +1,17 @@
+package converter.color.dos.com.colorconverter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dos on 10/23/2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+
+}
